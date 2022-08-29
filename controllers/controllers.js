17bc66008be60896612e7a5e2e3f63a88818428d
NathCoursey <ncoursey314@gmail.com>
2022-08-29 16:35:28 -0400

    show route added

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -8,12 +8,13 @@ router.use(express.urlencoded({ extended: false }));
 
 //MODEL IMPORT
 const db = require("../models");
-
+// new route
 //GET request for new posts template
 router.get("/new", (req, res) => {
     res.render("new.ejs");
   });
 
+// create route
 // POST request for adding new post to posts DB
 router.post('/', async (req, res) => {
     const createdPost = req.body;
@@ -22,6 +23,16 @@ router.post('/', async (req, res) => {
         console.log(newPost);
         res.redirect('/posts');
     } catch (err) {
+        console.log(err);
+    }
+})
+
+// show route
+router.get('/:postIndex', async (req, res) => {
+    try{
+        const foundPost = await db.Trips.findById(req.params.postIndex)
+        res.render('show.ejs', { post: foundPost, id: foundPost._id});
+    } catch(err) {
         console.log(err);
     }
 })
