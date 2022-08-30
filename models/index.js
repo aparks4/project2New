e585a3f4b76577741efac064bd905d26a5d27c0e
sjnay <samanthajnay@gmail.com>
2022-08-30 10:05:20 -0500

    user model

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 module.exports = {
     Cities : require("./Location.js"),
     Trips: require("./Trips.js"),
-    Collab: require("./Collab.js")
+    Collab: require("./Collab.js"),
+    User: require("./User.js")
 }
