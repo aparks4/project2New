d0b0553e5bdbba5ea58f1f266faae8a598885537
sjnay <samanthajnay@gmail.com>
2022-09-01 14:00:56 -0500

    city show page

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -42,9 +42,11 @@ router.post('/trips', async (req, res, next) => {
         let findCity = await Cities.findOne({city:newTrip.city, state:newTrip.state})
         if(!findCity){
            return await db.Cities.create({city:newTrip.city, state:newTrip.state, tripId:newTrip._id})
-        }else { await db.Cities.updateOne(
+        }else { await db.Cities.updateMany(
             {city:newTrip.city, state:newTrip.state},
-            {$addToSet:{tripId:newTrip._id}}
+            {$addToSet:{tripId:newTrip._id},
+            addToSet:{tripName:newTrip.tripName}},
+            
         )}
         res.redirect('/trips')
         console.log(findCity)
@@ -73,7 +75,7 @@ router.get('/trips/:tripIndex', async (req, res, next) => {
         const foundComments = await db.Collab.find({tripId: foundTrip._id});
         const foundUser = req.session.currentUser;
         const otherFoundUser = await db.User.findOne({username: foundUser.username});
-        console.log(foundComments);
+        //console.log(foundComments);
         res.render('show.ejs', { trip: foundTrip, id: foundTrip._id, comments: foundComments, user: otherFoundUser.username,});
     } catch(err) {
         console.log(err);
@@ -94,6 +96,19 @@ router.get('/trips', async (req, res, next) => {
         next()
     }
 })
+
+router.get('/trips/city', async (req, res, next) => {
+    try{
+        const allCities = await db.Cities.find()
+        const context= {cities: allCities}
+        console.log(allCities)
+        res.render('index-cities.ejs',context)
+    } catch(err) {
+        console.log(err);
+        next()
+    }
+})
+
 
 // destroy route
 router.delete('/trips/:tripIndex', async (req, res, next) => {
